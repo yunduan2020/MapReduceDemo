3834d2757b4eda627776271a553bfe12326eca70
yunduan2020 <72117684+yunduan2020@users.noreply.github.com>
2022-04-17 11:52:48 +0800

    Update TestETL.java

--- src/main/java/com/huxili/mapreduce/etl/TestETL.java
+++ src/main/java/com/huxili/mapreduce/etl/TestETL.java
@@ -1,5 +1,9 @@
 package com.huxili.mapreduce.etl;
 
+/*
+2022/04/17
+*/
+
 public class TestETL {
     public static void main(String[] args) {
         String check = "^((13[0-9])|(14[0-9])|(15[0-9])|(17[0-9])|(18[0-9]))\\d{8}$";
